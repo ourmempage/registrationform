222f1804e6d9a33e2f3099c7e5d6f63d40c858f7
ourmempage <149810754+ourmempage@users.noreply.github.com>
2024-02-14 16:26:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyobvsFiedmDV092pjK-xuu0F29m5APXYKAf0wMaQZFnmEurI4Cz194x12Ji07L5dSu/exec'
-
 const form = document.forms['contact-form']
-
 form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, {
