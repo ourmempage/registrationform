a9ea35190097381e4e3c13bf4f563109f3624c51
ourmempage <149810754+ourmempage@users.noreply.github.com>
2024-02-15 14:38:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.getElementById("myForm").addEventListener("submit", function(event) {
-    event.preventDefault();
+    event.preventDefault(); // Prevent default form submission behavior
+
     if (!validateForm()) {
         showErrorAlert();
         return;
@@ -11,7 +12,7 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
             // Remove the event listener after showing the success alert
-            document.getElementById("myForm").removeEventListener("submit", handleSubmit);
+            document.getElementById("myForm").reset(); // Reset form fields
         }
     };
     xhr.send(formData);
@@ -33,11 +34,25 @@ function showErrorAlert() {
     alertText.innerText = 'Please fill out all fields.';
     var customAlert = document.getElementById('customAlert');
     customAlert.style.display = 'block';
+
+    // Add click event listener to the document body to hide the alert box
+    document.body.addEventListener('click', hideAlertBoxOnce);
 }
 
 function showSuccessAlert() {
     var alertText = document.getElementById('alertText');
-    alertText.innerHTML = 'Thanks for the details. <strong>Our team will reach out soon.</strong>';
+    alertText.innerHTML = 'Thanks for the details. <strong><br>Our team will reach out soon.</strong>';
     var customAlert = document.getElementById('customAlert');
     customAlert.style.display = 'block';
+
+    // Add click event listener to the document body to hide the alert box
+    document.body.addEventListener('click', hideAlertBoxOnce);
+}
+
+function hideAlertBoxOnce() {
+    var customAlert = document.getElementById('customAlert');
+    customAlert.style.display = 'none';
+
+    // Remove the event listener after hiding the alert box once
+    document.body.removeEventListener('click', hideAlertBoxOnce);
 }
