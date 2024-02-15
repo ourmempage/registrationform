9c5b4ce30a0e64ab6fd101fcc43776b410052e7b
ourmempage <149810754+ourmempage@users.noreply.github.com>
2024-02-15 12:01:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbyobvsFiedmDV092pjK-xuu0F29m5APXYKAf0wMaQZFnmEurI4Cz194x12Ji07L5dSu/exec'
-const form = document.forms['contact-form']
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, {
-            method: 'POST',
-            body: new FormData(form)
-        })
-        .then(response => alert("Thank you! your form is submitted successfully."))
-        .then(() => {
-            window.location.reload();
-        })
-        .catch(error => console.error('Error!', error.message))
-})
-var location = e.parameter.location;
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    if (!validateForm()) {
+        showErrorAlert();
+        return;
+    }
+    showSuccessAlert();
+    var formData = new FormData(this);
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://script.google.com/macros/s/AKfycbyobvsFiedmDV092pjK-xuu0F29m5APXYKAf0wMaQZFnmEurI4Cz194x12Ji07L5dSu/exec", true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            // Remove the event listener after showing the success alert
+            document.getElementById("myForm").removeEventListener("submit", handleSubmit);
+        }
+    };
+    xhr.send(formData);
+});
+
+function validateForm() {
+    var name = document.getElementById('your-name').value;
+    var number = document.getElementById('your-number').value;
+    var email = document.getElementById('your-email').value;
+
+    if (name === "" || number === "" || email === "") {
+        return false;
+    }
+    return true;
+}
+
+function showErrorAlert() {
+    var alertText = document.getElementById('alertText');
+    alertText.innerText = 'Please fill out all fields.';
+    var customAlert = document.getElementById('customAlert');
+    customAlert.style.display = 'block';
+}
+
+function showSuccessAlert() {
+    var alertText = document.getElementById('alertText');
+    alertText.innerHTML = 'Thanks for the details. <strong>Our team will reach out soon.</strong>';
+    var customAlert = document.getElementById('customAlert');
+    customAlert.style.display = 'block';
+}
